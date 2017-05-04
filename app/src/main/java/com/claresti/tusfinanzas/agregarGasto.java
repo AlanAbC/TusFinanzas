ccef28b1b94c3bde521bfa44ecf2facef88cebd6
AlanAbC <alanjiraya77@gmail.com>
2017-05-04 11:48:34 -0500

    Se agrego Login

---
+++ app/src/main/java/com/claresti/tusfinanzas/agregarGasto.java
@@ -0,0 +1,13 @@
+package com.claresti.tusfinanzas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class agregarGasto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_gasto);
+    }
+}
