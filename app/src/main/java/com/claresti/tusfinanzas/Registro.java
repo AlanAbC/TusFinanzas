5fed6a4abaaa00db388062af33f79bb816863345
Elias Rosales <mansion1@hotmail.com>
2017-05-04 10:00:28 -0500

    Se creo la ventana de registro

---
+++ app/src/main/java/com/claresti/tusfinanzas/Registro.java
@@ -0,0 +1,19 @@
+package com.claresti.tusfinanzas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mansi on 04/05/2017.
+ */
+
+public class Registro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        //hola
+        setContentView(R.layout.activity_login);
+    }
+
+}
