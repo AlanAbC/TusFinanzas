9101fcc23118246ff6a6e9ec120facb01c9da189
Elias Rosales <mansion1@hotmail.com>
2017-05-04 10:09:27 -0500

    Se creo la ventana de registro

--- app/src/main/java/com/claresti/tusfinanzas/Login.java
+++ app/src/main/java/com/claresti/tusfinanzas/Login.java
@@ -1,15 +1,27 @@
 package com.claresti.tusfinanzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView btn_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         //hola
         setContentView(R.layout.activity_login);
+        btn_registro=(TextView)findViewById(R.id.btn_registro);
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,Registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
