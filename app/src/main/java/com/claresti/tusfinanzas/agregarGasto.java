96453304ef0ef0300aa99eb1919942ea4c9f92ac
AlanAbC <alanjiraya77@gmail.com>
2017-05-10 11:05:51 -0500

    Se creo agregar categoria y presupuestos

--- app/src/main/java/com/claresti/tusfinanzas/agregarGasto.java
+++ app/src/main/java/com/claresti/tusfinanzas/agregarGasto.java
@@ -1,14 +1,73 @@
 package com.claresti.tusfinanzas;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class agregarGasto extends AppCompatActivity {
-
+    //Menu, Declaracion de variables
+    private DrawerLayout drawerLayout;
+    final List<MenuItem> items = new ArrayList<>();
+    private Menu menu;
+    private ImageView btnMenu;
+    private NavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_agregar_gasto);
+        menuNav();
+    }
+    /**
+     * Funcion que da funcionalidad al menu
+     */
+    private void menuNav(){
+        for(int i = 0; i < menu.size(); i++){
+            items.add(menu.getItem(i));
+        }
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                int pos = items.indexOf(item);
+                if(pos == 0){
+
+                }else if(pos == 1){
+                    Intent i = new Intent(agregarGasto.this, agregarGasto.class);
+                    startActivity(i);
+                }else if(pos == 2){
+                    Intent i = new Intent(agregarGasto.this, agregar_categoria.class);
+                    startActivity(i);
+                }else if(pos == 3) {
+                    Intent i = new Intent(agregarGasto.this, presupuestos.class);
+                    startActivity(i);
+                }else if(pos == 4){
+                    Intent i = new Intent(agregarGasto.this, Registro.class);
+                    startActivity(i);
+                }else if(pos == 5){
+                    Intent i = new Intent(agregarGasto.this, acerca.class);
+                    startActivity(i);
+                }
+                drawerLayout.closeDrawer(nav);
+                item.setChecked(false);
+                return false;
+            }
+        });
     }
 }
